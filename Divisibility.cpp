98c9ad4ec5d73c1264782601405551e136204870
Sumitra Dawn <64164353+SUMITRA021@users.noreply.github.com>
2022-01-17 21:36:21 +0530

    Update Divisibility.cpp

--- Divisibility.cpp
+++ Divisibility.cpp
@@ -1,19 +1,26 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int main()
-{
-	
-	int a,n,arr[100];
-	cin>>n;
-	for(int i=0;i<n;i++)
-	{
-		cin>>arr[i];
-	}
 
-	if(arr[n-1]%10 == 0)
-		cout<<"Yes";
-	else
-	 	cout<<"No";
+ 
+
+int main() { int n; cin>>n;
+
+int a[n], t=n ;
+
+while(t)
+
+{
+
+cin>>a[n-t]; t--;
+
+}
+
+if( a[n-1]%10==0 ) cout<<"Yes"; else cout<<"No";
+
+ 
+
+return 0;
 
 }
